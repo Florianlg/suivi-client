88442a35d6703a4aae94d4467653963a4901d231
Florian <contact@empreintesdigitales.fr>
2024-11-25 16:37:58 +0100

    add Auth0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-
+import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Button, Container } from "@mui/material";
+import { AppBar, Toolbar, Button, Container, Typography } from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react"; // Import Auth0
+
 import Home from "./pages/Home";
 import Prestations from "./pages/Prestations";
 import Stats from "./pages/Stats";
@@ -9,40 +11,86 @@ import ClientDetails from "./pages/ClientDetails";
 import MentalPreparation from "./pages/MentalPreparation";
 
 const App = () => {
+  const { loginWithRedirect, logout, isAuthenticated, user, isLoading } =
+    useAuth0();
+
+  // Affichage d'un écran de chargement pendant l'authentification
+  if (isLoading) {
+    return (
+      <Container style={{ marginTop: "20px", textAlign: "center" }}>
+        <Typography variant="h5">Chargement...</Typography>
+      </Container>
+    );
+  }
+
   return (
     <>
       {/* Barre de navigation */}
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            Ajouter une prestation
-          </Button>
-          <Button color="inherit" component={Link} to="/prestations">
-            Voir les prestations
-          </Button>
-          <Button color="inherit" component={Link} to="/stats">
-            Statistiques
-          </Button>
-          <Button color="inherit" component={Link} to="/objectifs">
-            Objectifs
-          </Button>
-          <Button color="inherit" component={Link} to="/mental-preparation">
-            Préparation mentale
-          </Button>
+          {isAuthenticated ? (
+            <>
+              <Button color="inherit" component={Link} to="/">
+                Ajouter une prestation
+              </Button>
+              <Button color="inherit" component={Link} to="/prestations">
+                Voir les prestations
+              </Button>
+              <Button color="inherit" component={Link} to="/stats">
+                Statistiques
+              </Button>
+              <Button color="inherit" component={Link} to="/objectifs">
+                Objectifs
+              </Button>
+              <Button color="inherit" component={Link} to="/mental-preparation">
+                Préparation mentale
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() =>
+                  logout({ returnTo: window.location.origin })
+                }
+              >
+                Déconnexion
+              </Button>
+              <Typography sx={{ ml: 2, fontSize: 16 }}>
+                Bonjour, {user?.name}
+              </Typography>
+            </>
+          ) : (
+            <Button
+              color="inherit"
+              onClick={() => loginWithRedirect()}
+            >
+              Connexion
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
 
       {/* Contenu principal */}
       <Container style={{ marginTop: "20px" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/prestations" element={<Prestations />} />
-          <Route path="/stats" element={<Stats />} />
-          <Route path="/objectifs" element={<Objectifs />} />
-          <Route path="/mental-preparation" element={<MentalPreparation />} />
-          <Route path="/client/:clientName" element={<ClientDetails />} />
-          <Route path="*" element={<div>Page non trouvée</div>} />
-        </Routes>
+        {isAuthenticated ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/prestations" element={<Prestations />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/objectifs" element={<Objectifs />} />
+            <Route
+              path="/mental-preparation"
+              element={<MentalPreparation />}
+            />
+            <Route
+              path="/client/:clientName"
+              element={<ClientDetails />}
+            />
+            <Route path="*" element={<div>Page non trouvée</div>} />
+          </Routes>
+        ) : (
+          <Typography variant="h6" align="center">
+            Veuillez vous connecter pour accéder au contenu.
+          </Typography>
+        )}
       </Container>
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Assurez-vous que ce composant est importé
-import App from "./App"; // Importez le fichier App
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react"; // Import Auth0Provider
+import App from "./App"; // Import App
+
+// Informations Auth0 (remplacez par vos valeurs)
+const domain = "dev-bf30f2spk3jpcvs5.uk.auth0.com";
+const clientId = "Thx4YCT23bc7dK4MYaucfjMRgCudIXHz";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* Fournir le contexte Auth0 à toute l'application */}
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin, // Redirection après authentification
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
