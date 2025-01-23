344ac44cf63292eaf96f4a6b41cbc64229481daf
Florian <contact@empreintesdigitales.fr>
2025-01-23 14:43:41 +0100

    deploiement FE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config(); // Charge les variables du fichier .env
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false, // Désactive la minification pour un débogage plus facile (optionnel)
+    minify: false, // Désactiver la minification pour déboguer plus facilement
   },
 });
+
+console.log(import.meta.env); // Pour vérifier les variables chargées
