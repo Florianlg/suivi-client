20090615703a2254b6f1756bfb335885aef9858b
Florian <contact@empreintesdigitales.fr>
2025-01-27 14:56:42 +0100

    deploiement FE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ import {
     FormControlLabel,
 } from "@mui/material";
 
-const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:4000";
+const API_BASE_URL = process.env.VITE_API_BASE_URL; // || "http://localhost:4000"
 
 
 const Home = () => {
