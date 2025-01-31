8bc14ca2f8209d8d34635ad614d1572f66a190a2
Florian <contact@empreintesdigitales.fr>
2025-01-31 15:23:36 +0100

    Modif import theme

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -1,4 +1,11 @@
-const theme = {
+import { createTheme } from "@mui/material/styles";
+import typography from "./typography";
+import colors from "./colors";
+
+
+const theme = createTheme({
+  typography, // Intègre la typographie
+  palette: colors, // Intègre la palette de couleurs
   borderRadius: "8px",
   shadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
   font: {
